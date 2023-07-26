81b7325230bf4b9b194fcb607a6a87dd6c74d284
JParedesArtica <126907318+JParedesArtica@users.noreply.github.com>
2023-07-26 20:07:38 +0000

    traficc light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	
+	
+		const [color, setColor] = useState("null");
+		
+
+		let redExtraClass = " ";
+		if(color === "red") redExtraClass = "selected"
+		let yellowExtraClass = " ";
+		if(color === "yellow") yellowExtraClass = "selected"
+		let greenExtraClass = " ";
+		if(color === "green") greenExtraClass = "selected"
+
+		
+
+		return (
+			<div className="container">
+			<div id = "trafficTop"></div>
+			<div id="semaforo">
+			<div className = {"red light " + redExtraClass} onClick={()=>{setColor("red")}}></div>
+			<div className={"yellow light " + yellowExtraClass} onClick={()=>{setColor("yellow")}}></div>
+			<div className={"green light " + greenExtraClass} onClick={()=>{setColor("green")}}></div>
+			</div>
+			
+			</div>
+			
+		  
+		);
+	  };
+	  
 
 export default Home;
